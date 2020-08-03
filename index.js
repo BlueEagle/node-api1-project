93142ac22b7512455e1b287a00137fb0970407a1
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-03 15:14:51 -0600

    works 100%

--- index.js
+++ index.js
@@ -35,38 +35,82 @@ server.post("/api/users", (req, res) => {
     users.push(user);
     res.status(201).json(users);
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        errorMessage:
-          "There was an error while saving the user to the database",
-      });
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
   }
 });
 
 server.get("/api/users", (req, res) => {
-  res.send(users);
+  try {
+    res.send(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ errorMessage: "The users information could not be retrieved." });
+  }
 });
 
 server.get("/api/users/:id", (req, res) => {
-  res.send(users.filter((user) => user.id === req.params.id));
+  try {
+    let userFound = users.filter((user) => user.id === req.params.id);
+    if (!userFound.name)
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    res.send(userFound);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ errorMessage: "The user information could not be retrieved." });
+  }
 });
 
 server.delete("/api/users/:id", (req, res) => {
-  users = users.filter((user) => user.id !== req.params.id);
-  res.status(200).end();
+  try {
+    let userExists = false;
+    users = users.filter((user) => {
+      console.log(user.id);
+      if (user.id === req.params.id) userExists = !userExists;
+      return user.id !== req.params.id;
+    });
+    if (!userExists)
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    res.status(200).end();
+  } catch (err) {
+    res.status(500).json({ errorMessage: "The user could not be removed" });
+  }
 });
 
 server.put("/api/users/:id", (req, res) => {
-  users = users.map((user) => {
-    if (user.id === req.params.id) {
-      user = req.body;
-      user.id = nanoid();
-      return user;
+  try {
+    if (req.body.name === undefined || req.body.bio === undefined) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
     }
-    return user;
-  });
-  res.status(200).json(users);
+    let userFound = false;
+    users = users.map((user) => {
+      if (user.id === req.params.id) {
+        userFound = true;
+        user = req.body;
+        user.id = nanoid();
+        return user;
+      }
+      return user;
+    });
+    if (!userFound)
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    res.status(200).json(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ errorMessage: "The user information could not be modified." });
+  }
 });
 
 const port = 8000;
